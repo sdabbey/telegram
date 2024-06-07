bdf109ba937d5d5946b1d5999ce4d924222f48d0
sdabbey <dehydrus223@gmail.com>
2024-06-07 03:19:34 +0000

    Connect user avatar to stream chat

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { router } from "expo-router";
 import React from "react";
 import { ChannelList } from "stream-chat-expo";
+import { useAuth } from "../../../providers/AuthProvider";
 
 export default function MainTabScreen(){
-    
-    return <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />;
+    const { user } = useAuth();
+    return <ChannelList 
+        filters={{members: {$in: [user.id]}}}
+        onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />;
     
 }
